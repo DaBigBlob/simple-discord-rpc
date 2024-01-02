a97ea1c50f58521b4a95ecc5b5ed881c44a34be1
Person <localboxcrox@gmail.com>
2024-01-02 04:20:56 -0500

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use discord_rich_presence::{activity, DiscordIpc, DiscordIpcClient};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = DiscordIpcClient::new("<some application ID>")?;
+
+    client.connect()?;
+    client.set_activity(activity::Activity::new()
+        .state("foo")
+        .details("bar")
+    )?;
+    client.close()?;
+
+    Ok(())
 }
