9ed06a3238b19f0f0612333a39aa386afb27937c
Person <localboxcrox@gmail.com>
2024-01-02 04:46:23 -0500

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 use discord_rich_presence::{activity, DiscordIpc, DiscordIpcClient};
+use std::error::Error;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = DiscordIpcClient::new("<some application ID>")?;
-
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut client = DiscordIpcClient::new("771124766517755954")?;
     client.connect()?;
+    
     client.set_activity(activity::Activity::new()
-        .state("foo")
-        .details("bar")
-    )?;
-    client.close()?;
+    .state("A test")
+    .details("A placeholder")
+    // .buttons(vec![activity::Button::new(
+    //     "A button",
+    //     "https://github.com",
+    // )])
+    .assets(
+        activity::Assets::new()
+            .large_image("https://logseq.com/logo-with-border.5bf84f43.png")
+            .large_text("lol")
+            .small_image("https://logseq.com/logo-with-border.5bf84f43.png")
+            .small_text("lol")
+    ))?;
 
+    std::thread::park();
+
+    client.close()?;
     Ok(())
 }
